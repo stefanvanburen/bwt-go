d04851b7b335ccf28c118241146df7fcab379e57
Stefan VanBuren <me@svbn.me>
2021-02-22 13:45:12 -0500

    test: Refactor to idiomatic table-driven tests
    Also, adds a test-case found on wikipedia.

--- bwt_test.go
+++ bwt_test.go
@@ -6,44 +6,49 @@ import (
 	"github.com/matryer/is"
 )
 
-var NaiveBWTtests = []struct {
-	in  string
-	out string
-}{
-	{"hey", "yh$e"},
-	{"banana", "annb$aa"},
-	{"appellee", "e$elplepa"},
-	{"dogwood", "do$oodwg"},
-}
-
 func TestNaiveBWT(t *testing.T) {
-	is := is.New(t)
+	tests := map[string]struct {
+		in  string
+		out string
+	}{
+		"hey":      {"hey", "yh$e"},
+		"banana":   {"banana", "annb$aa"},
+		"appellee": {"appellee", "e$elplepa"},
+		"dogwood":  {"dogwood", "do$oodwg"},
+	}
 
-	for _, tt := range NaiveBWTtests {
-		is.Equal(NaiveBWT(tt.in), tt.out)
-		is.Equal(NaiveInverseBWT(tt.out), tt.in)
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			is := is.New(t)
+
+			is.Equal(NaiveBWT(tt.in), tt.out)
+			is.Equal(NaiveInverseBWT(tt.out), tt.in)
+		})
 	}
 }
 
-var BWTtests = []struct {
-	in  string
-	out string
-}{
-	{"hey", "hye"},
-	{"banana", "nnbaaa"},
-	{"appellee", "eelplepa"},
-	{"dogwood", "odoodwg"},
-}
-
 func TestBWT(t *testing.T) {
-	is := is.New(t)
+	tests := map[string]struct {
+		in  string
+		out string
+	}{
+		"hey":       {"hey", "hye"},
+		"banana":    {"banana", "nnbaaa"},
+		"appellee":  {"appellee", "eelplepa"},
+		"dogwood":   {"dogwood", "odoodwg"},
+		"wikipedia": {"SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES", "TEXYDST.E.IXIXIXXSSMPPS.B..E.S.EUSFXDIIOIIIT"},
+	}
 
-	for _, tt := range BWTtests {
-		o, _ := BWT(tt.in)
-		is.Equal(o, tt.out)
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			is := is.New(t)
 
-		// TODO: this method doesn't work
-		// i := IBWT(tt.out, il)
-		// assert.Equal(t, tt.in, i)
+			o, _ := BWT(tt.in)
+			is.Equal(o, tt.out)
+
+			// TODO: this method doesn't work
+			// i := InverseBWT(tt.out, il)
+			// is.Equal(tt.in, i)
+		})
 	}
 }
