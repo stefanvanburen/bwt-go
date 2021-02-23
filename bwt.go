26c3b077f40df66ff45aa18581c7064e534b7f24
Stefan VanBuren <me@svbn.me>
2021-02-23 09:19:17 -0500

    refactor: Trim down package to just naive variants
    Played around with a python implementation using index lists [1], which
    must have been where I translated the original code, and it seems wrong;
    if I try putting in "hey" and run in through `bwt` and `ibwt` with the
    generated indexlist, it doesn't get back to "hey". I could never quite
    get the inverse to work myself, and I haven't found a great paper
    describing the index list method more thoroughly for now, so remove it
    and just keep around the naive versions with an exposed EOFChar that can
    be modified by callers.
    
    [1]: https://learn.fmi.uni-sofia.bg/pluginfile.php/169284/mod_resource/content/1/Burrows---Wheeler_transform.pdf

--- bwt.go
+++ bwt.go
@@ -8,18 +8,20 @@ import (
 	"strings"
 )
 
-const eofChar = "$"
+var EOFChar = "$"
 
-// NaiveBWT returns the result of the Burrows-Wheeler Transform on a string
-func NaiveBWT(s string) string {
-	s = s + eofChar
+// BWT returns the result of the Burrows-Wheeler Transform on a string, using
+// the EOFChar as a marker.
+func BWT(s string) string {
+	s = s + EOFChar
 	x := getRotations(s)
 	sort.Strings(x)
 	return takeLast(x)
 }
 
-// NaiveInverseBWT returns the inverse of the Burrows-Wheeler Transform on a string
-func NaiveInverseBWT(s string) string {
+// InverseBWT returns the inverse of the Burrows-Wheeler Transform on a string,
+// using the EOFChar as a marker.
+func InverseBWT(s string) string {
 	x := strings.Split(s, "")
 	z := make([]string, len(s))
 	for i := 0; i < len(x); i++ {
@@ -29,58 +31,6 @@ func NaiveInverseBWT(s string) string {
 		sort.Strings(z)
 	}
 	return findLast(z)
-}
-
-// IndexList is used to lower the complexity of the InverseBWT operation.
-type IndexList []int
-
-// BWT creates an indexlist for use with InverseBWT.
-func BWT(s string) (string, IndexList) {
-	x := getRotations(s)
-	y := make([]string, len(x))
-	copy(y, x)
-	sort.Strings(y)
-	il := make(IndexList, len(s))
-	l := len(s)
-	for i, v := range y {
-		i1 := index(v, x)
-		if i1 < l-1 {
-			i1++
-		} else {
-			i1 = 0
-		}
-		i2 := index(x[i1], y)
-		il[i] = i2
-	}
-	return takeLast(y), il
-}
-
-// InverseBWT returns the inverse of a BWT transformation, using an index list.
-func InverseBWT(r string, il IndexList) string {
-	s := ""
-	i := il[0]
-	for z := 0; z < len(r); z++ {
-		s = s + string(r[i])
-		i = il[i]
-	}
-	return s
-}
-
-// MTF returns the Move-To-Front Coding of a string
-// Typically used on a Burrows-Wheeler transformed string
-func MTF(s string) []int {
-	a := alphabet(s)
-	mtf := make([]int, len(s))
-	for i := 0; i < len(s); i++ {
-		// Find the index of s[i]
-		j := strings.Index(a, string(s[i]))
-		mtf = append(mtf, j)
-		// Move it to the front
-		char := string(a[j])
-		a = a[:j] + a[j+1:]
-		a = char + a
-	}
-	return mtf
 }
 
 func rotate(s string) string {
@@ -104,39 +54,12 @@ func takeLast(ss []string) string {
 	return z
 }
 
-func index(s string, ss []string) int {
-	for i, e := range ss {
-		if strings.Compare(s, e) == 0 {
-			return i
-		}
-	}
-	return -1
-}
-
 func findLast(ss []string) string {
 	for _, e := range ss {
-		if string(e[len(e)-1]) == eofChar {
+		if string(e[len(e)-1]) == EOFChar {
 			// Return the string up until the eofChar
 			return string(e[:len(e)-1])
 		}
 	}
 	return ""
-}
-
-// Get a sorted alphabet of all the characters in the string
-func alphabet(s string) string {
-	set := make(map[string]bool)
-	for i := 0; i < len(s); i++ {
-		set[string(s[i])] = true
-	}
-	ss := make([]string, len(s))
-	for k := range set {
-		ss = append(ss, k)
-	}
-	sort.Strings(ss)
-	r := ""
-	for _, v := range ss {
-		r += v
-	}
-	return r
 }
--- bwt_test.go
+++ bwt_test.go
@@ -21,34 +21,8 @@ func TestNaiveBWT(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			is := is.New(t)
 
-			is.Equal(NaiveBWT(tt.in), tt.out)
-			is.Equal(NaiveInverseBWT(tt.out), tt.in)
-		})
-	}
-}
-
-func TestBWT(t *testing.T) {
-	tests := map[string]struct {
-		in  string
-		out string
-	}{
-		"hey":       {"hey", "hye"},
-		"banana":    {"banana", "nnbaaa"},
-		"appellee":  {"appellee", "eelplepa"},
-		"dogwood":   {"dogwood", "odoodwg"},
-		"wikipedia": {"SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES", "TEXYDST.E.IXIXIXXSSMPPS.B..E.S.EUSFXDIIOIIIT"},
-	}
-
-	for name, tt := range tests {
-		t.Run(name, func(t *testing.T) {
-			is := is.New(t)
-
-			o, _ := BWT(tt.in)
-			is.Equal(o, tt.out)
-
-			// TODO: this method doesn't work
-			// i := InverseBWT(tt.out, il)
-			// is.Equal(tt.in, i)
+			is.Equal(BWT(tt.in), tt.out)
+			is.Equal(InverseBWT(tt.out), tt.in)
 		})
 	}
 }
